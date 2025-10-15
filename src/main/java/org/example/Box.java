612df0d6a9f3421a624dffd752ba8edb2955b37e
Artur Hohrin <artur.hohrin@mail.ru>
2025-10-15 18:25:36 +0400

    изменена логика новый класс

---
+++ src/main/java/org/example/Box.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public Box(){
+
+    }
+    public Box(int size) {
+        this.size = size;
+    }
+}
