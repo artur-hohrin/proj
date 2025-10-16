8e905a2b9cefa24c59854845985f31fb4ba9e01f
Artur Hohrin <artur.hohrin@mail.ru>
2025-10-16 17:55:24 +0400

    добавлен вызов метода doSmthng

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("--------");
         System.out.println("Привет мир!");
+        doSomething();
         System.out.println("0");
     }
     public static void doSomething(){
