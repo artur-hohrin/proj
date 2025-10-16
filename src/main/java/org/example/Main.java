f6c139893d6ec819ce748747da4d5cb340f8cad3
Artur Hohrin <artur.hohrin@mail.ru>
2025-10-16 17:00:57 +0400

    улучшили логику

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         System.out.println("43252352");
+        System.out.println("65463");
     }
 }
